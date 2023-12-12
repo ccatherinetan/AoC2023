3e7ec4acab29a10669ce0392df889d98b575af42
ccatherinetan <catherine95118@gmail.com>
2023-12-12 00:04:44 -0800

    complete day 1, problem 1

---
+++ AoC2023/src/Main.java
@@ -0,0 +1,63 @@
+import edu.princeton.cs.algs4.In;
+
+import java.util.HashMap;
+
+public class Main {
+    public static HashMap<String, Integer> digitMap = new HashMap<>();
+    public static void main(String[] args) {
+        readFile("src/data/data.txt");
+        System.out.println("Hello world!");
+    }
+
+    static void updateDigitMap() {
+        // digitMap.clear();
+        digitMap.put("one", 1);
+        digitMap.put("two", 2);
+        digitMap.put("three", 3);
+        digitMap.put("four", 4);
+        digitMap.put("five", 5);
+        digitMap.put("six", 6);
+        digitMap.put("seven", 7);
+        digitMap.put("eight", 8);
+        digitMap.put("nine", 9);
+    }
+
+    public static void readFile(String file) {
+        updateDigitMap();
+        In in = new In(file);
+        int sum = 0;
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            sum += findCalibrationValue(line);
+        }
+        System.out.println("Sum: " + sum);
+    }
+
+    static int findFirstInt(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= '0' && c <= '9') {
+                return Integer.parseInt(String.valueOf(c));
+            }
+        }
+        return -1;
+    }
+
+    static int findSecondInt(String s) {
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c >= '0' && c <= '9') {
+                return Integer.parseInt(String.valueOf(c));
+            }
+        }
+        return -1;
+    }
+
+    static int findCalibrationValue(String s) {
+        //find firstInt
+        int firstInt = findFirstInt(s);
+        int secondInt = findSecondInt(s);
+
+        return firstInt * 10 + secondInt;
+    }
+}
