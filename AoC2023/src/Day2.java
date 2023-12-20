c66df1cf53b06c75982357210a7bbafd887181df
ccatherinetan <catherine95118@gmail.com>
2023-12-19 16:03:20 -0800

    pass day2 part1

--- AoC2023/src/Day2.java
+++ AoC2023/src/Day2.java
@@ -14,6 +14,7 @@ public class Day2 {
 //    public final static int MAX_RED = 12;
 //    public final static int MAX_GREEN = 13;
 //    public final static int MAX_BLUE = 14;
+    public final String[] colors = new String[]{"red", "green", "blue"};
     public Day2(String file) {
         RGBbyGame = new HashMap<>();
         readFile(file);
@@ -22,12 +23,20 @@ public class Day2 {
         In in = new In(file);
         while (in.hasNextLine()) {
             int[] maxRGBValues = new int[3];
+            // get gameNum of current game
             String[] line = in.readLine().split(": ");
             int gameNum = getGameNum(line[0]);
-            String[] gameSets = line[1].split("; ");
-            for (int i = 0; i < 3; i++) {
-                maxRGBValues = udpateMaxRGBValues(gameSets[i], maxRGBValues);
+
+            // iterate through each of the 3 rounds for each game
+            String[] colorSets = line[1].split("[;,] ");
+            for (int i = 0; i < colorSets.length; i++) {
+                String oneSet = colorSets[i];
+                maxRGBValues = updateNumber(oneSet, maxRGBValues);
             }
+//            String[] gameSets = line[1].split("; ");
+//            for (int i = 0; i < 3; i++) {
+//                maxRGBValues = handleOneGame(gameSets[i], maxRGBValues);
+//            }
             RGBbyGame.put(gameNum, maxRGBValues);
         }
     }
@@ -36,18 +45,45 @@ public class Day2 {
         return Integer.parseInt(s.substring(5));
     }
 
-    private int[] udpateMaxRGBValues(String s, int[] tobeUpdated) {
-        Pattern pattern = Pattern.compile("\\b(\\d+)\\b");
-        Matcher matcher = pattern.matcher(s);
+    /**
+     * Update the maxVals array with the current grab.
+     * @param currGrab: string, in the form of "[no.] [color]" e.g. "5 red"
+     * @param tobeUpdated an array of max values of each color, RGB in that order
+     * @return tobeUpdated
+     */
+    private int[] updateNumber(String currGrab, int[] tobeUpdated) {
         int i = 0;
-        while (matcher.find()) { // should only be 3
-            int number = Integer.parseInt(matcher.group(1));
-            if (number > tobeUpdated[i]) {
-                tobeUpdated[i] = number;
+        for (String color: colors) {
+            int stopIndex = currGrab.indexOf(color);
+            if (stopIndex != -1) {
+                int number = Integer.parseInt(currGrab.substring(0, stopIndex - 1));
+                if (number > tobeUpdated[i]) {
+                    tobeUpdated[i] = number;
+                }
             }
             i++;
         }
         return tobeUpdated;
+    }
+
+    private int[] handleOneGame(String s, int[] tobeUpdated) {
+        String[] colorSets = s.split(", ");
+        for (int j = 0; j < colorSets.length; j++) {
+            tobeUpdated = updateNumber(colorSets[j], tobeUpdated);
+        }
+        return tobeUpdated;
+
+//        Pattern pattern = Pattern.compile("\\b(\\d+)\\b");
+//        Matcher matcher = pattern.matcher(s);
+//        int i = 0;
+//        while (matcher.find()) { // should only be 3
+//            int number = Integer.parseInt(matcher.group(1));
+//            if (number > tobeUpdated[i]) {
+//                tobeUpdated[i] = number;
+//            }
+//            i++;
+//        }
+//        return tobeUpdated;
     }
 
     private boolean isValid(int[] vals) {
@@ -62,6 +98,7 @@ public class Day2 {
         int sum = 0;
         for (int key : RGBbyGame.keySet()) {
             if (isValid(RGBbyGame.get(key))) {
+                // System.out.println(key);
                 sum += key;
             }
         }
