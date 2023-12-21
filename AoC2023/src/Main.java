202c720099c717d6842291497da32939426348bc
ccatherinetan <catherine95118@gmail.com>
2023-12-20 17:48:46 -0800

    rename data files

--- AoC2023/src/Main.java
+++ AoC2023/src/Main.java
@@ -12,13 +12,13 @@ public class Main {
       Day1 d = new Day1(file);
 
     DAY 2:
-     String file = "src/data/day2part1.txt";
+     String file = "src/data/day2.txt";
      Day2 d = new Day2(file);
      System.out.println(d.returnSum());
      System.out.println(d.returnPowerSum());
 
     DAY 3:
-     String file = "src/data/day3part1.txt";
+     String file = "src/data/day3.txt";
      Day3 d = new Day3(file);
      System.out.println(d.returnSum());
      System.out.println(d.returnGearSum());
